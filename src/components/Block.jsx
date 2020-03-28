0ea10ee5262859239cb05e1d636861a7312e64f9
fdavison1 <fred.noreply@gmail.com>
2020-03-28 14:35:12 -0500

    wireframe work

--- src/components/Block.jsx
+++ src/components/Block.jsx
@@ -4,25 +4,22 @@ export const Block = () => {
     return(
         <div className='block'>
             <section>
-                <h4>HEALTH</h4>
-                <p>eat food</p>
+                <h4>FOOD</h4>
+                <p><input type='checkbox'/>eat real food</p>
             </section>
             <section>
                 <h4>ADMIN</h4>
-                <p>yellow task</p>
-                <input type="text"/>
+                <p><input type='checkbox'/>complete yellow task</p>
             </section>
             <section>
-                <h4>HEALTH</h4>
-                <p>work out</p>
+                <h4>MUSCLES</h4>
+                <p><input type='checkbox'/>build work out habit</p>
             </section>
             <section>
                 <h4>CODING</h4>
-                <p>project commit</p>
-                <br/>
+                <p><input type='checkbox'/>make project commit</p>
                 <p>OR</p>
-                <br/>
-                <p>tutorial notes</p>
+                <p><input type='checkbox'/>take tutorial notes</p>
             </section>
         </div>
     )
